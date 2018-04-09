4564f0967df6223faf4419a50eeb28c5dbe6e64a
Przemek <ememek96@gmail.com>
2018-04-09 15:02:41 +0200

    Dodaj pliki projektów.

---
+++ si_constraints/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace si_constraints
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
